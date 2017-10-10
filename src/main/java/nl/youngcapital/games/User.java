91235228facfb7891d5c909fdb6978cdd2a207a2
Student <Student@C0035.isential.local>
2017-10-10 16:09:34 +0200

    Versie met werkende home, POST en GET

--- src/main/java/nl/youngcapital/games/User.java
+++ src/main/java/nl/youngcapital/games/User.java
@@ -1,8 +1,5 @@
 package nl.youngcapital.games;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
