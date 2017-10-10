3b4b187cd653401bbb09b32ae38767f75d081054
Bas <basns1993@gmail.com>
2017-10-10 15:02:12 +0200

    post fixed

--- src/main/java/nl/youngcapital/games/rest/GameEndPoint.java
+++ src/main/java/nl/youngcapital/games/rest/GameEndPoint.java
@@ -2,9 +2,12 @@ package nl.youngcapital.games.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.youngcapital.games.model.Game;
+import nl.youngcapital.games.model.TypeGame;
 import nl.youngcapital.games.service.GameService;
 
 @RestController
@@ -17,5 +20,19 @@ public class GameEndPoint {
 		Game game = new Game();
 		gameService.test(game);
 		return game;
+	}
+	@GetMapping("/NewFile")
+	public Game getGame2() {
+		Game game = new Game();
+		game.setTypeGame(TypeGame.HANGMAN);
+		gameService.test(game);
+		return game;
+	}
+	@PostMapping("/gamepost")
+	public void postEntiteit(@RequestBody Game game) {
+		System.out.print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		System.out.println("Jojo");
+		System.out.println(game.getTypeGame());
+		gameService.test(game);
 	}
 }
