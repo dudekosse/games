01bfabf54c0ac7e3668909629e5751af63a8b0f8
Bas <basns1993@gmail.com>
2017-10-11 13:35:23 +0200

    quick inbetween

--- src/main/resources/static/js/postGame.js
+++ src/main/resources/static/js/postGame.js
@@ -1,11 +1,11 @@
 function postGame(game,plays,wins,user){
 	//var ent= document.getElementById("gamename").value;
 	//var game = '{"name":"'+ent+'"}'; 
-    var gameInfo = '{"typeGame:" '+game+',"timesPlayed:" '+plays+',"user:"'+user+'"highscore"'+wins+'}';
-
+    var gameInfo = '{"typeGame": "'+game+'","timesPlayed": "'+plays+'","user":"'+user+'","highscore":"'+wins+'"}';
+    
+   // var gameInfo = '{"typeGame:" +game+,"timesPlayed:" +plays+,"user:"+user+"highscore"+wins+};
+    console.log(gameInfo);
 	var xhttp = new XMLHttpRequest();
-
-	alert(gameInfo);
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 202) {
 			document.getElementById("outputserver").innerHTML = this.responseText;
@@ -14,6 +14,6 @@ function postGame(game,plays,wins,user){
 
 	xhttp.open("POST", "http://localhost:8082/gamepost", true);
 	xhttp.setRequestHeader("Content-type", "application/json");
-	xhttp.send(game);					
+	xhttp.send(gameInfo);					
 
 }
