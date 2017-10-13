9ba5346e7392d69c8a7f2fd3f791821d2b08ed5a
Student <Student@C0035.isential.local>
2017-10-13 11:30:50 +0200

    Date of birth and Country drop down implemented

--- src/main/resources/static/js/postUser.js
+++ src/main/resources/static/js/postUser.js
@@ -1,9 +1,24 @@
+function calcAge(dateofbirth) {
+	var today = new Date();
+    var dateofbirth = new Date(dateofbirth);
+    var age = today.getFullYear() - dateofbirth.getFullYear();
+    var mon = today.getMonth() - dateofbirth.getMonth();
+    if (mon < 0 || (mon === 0 && today.getDate() < dateofbirth.getDate())) {
+        age--;
+    }
+    return age;
+}
+
 function postUser(){
 	var username= document.getElementById("username").value;
 	var password= document.getElementById("password").value;
-	var age= document.getElementById("age").value;
+	var day = document.getElementById("day").value;
+	var month= document.getElementById("month").value;
+	var year = document.getElementById("year").value;
+	var datestring = String(month)+"-"+String(day)+"-"+String(year);
+	var age = calcAge(datestring)
 	var country= document.getElementById("country").value;
-    
+	
 	var user = '{"username": "'+username+'","password": "'+password+'","age":"'+age+'","country":"'+country+'"}';
 	var xhttp = new XMLHttpRequest();
 	
@@ -15,4 +30,5 @@ function postUser(){
 	xhttp.open("POST", "http://localhost:8082/registerpost", true);
 	xhttp.setRequestHeader("Content-type", "application/json");
 	xhttp.send(user);	
+	location.href = "success.html"
 }
